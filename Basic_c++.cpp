5b92549c63a10cfa69d6e80880879a353ec54a30
Sanjay J <148109515+Sanjayj-06@users.noreply.github.com>
2025-01-06 12:37:03 +0530

    Update Basic_c++.cpp

--- Basic_c++.cpp
+++ Basic_c++.cpp
@@ -1,1 +1,19 @@
+/* input and output statements */
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,y;
+    cout << "Enter x and y value : ";
+    cin >> x >> y;
+    cout << "The value of x is  " << x <<" and y is " << y;
+    return 0;
+}
+
+/* output 
+Enter x and y value : 10 12 
+The value of x is  10 and y is 12
+*/
+
+
+  
