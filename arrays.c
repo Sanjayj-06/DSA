f49685bf25917b4c52b2eb9febbb420f5c63357c
Sanjay J <148109515+Sanjayj-06@users.noreply.github.com>
2025-01-13 21:50:53 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -1,1 +1,12 @@
+/*largest element in a array*/
+int max;
+max = arr[0];
+int i;
+for (i = 0; i < n; i++) { // Loop goes from 0 to n-1 (exclusive)
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
+return max;
 
+/*second largest element in a array*/
